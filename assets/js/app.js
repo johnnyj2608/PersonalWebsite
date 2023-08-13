9b52e0ff565041fbf843b375e64028fc0a0d2796
Johnny Jiang <johnnyj2608@gmail.com>
2023-08-12 23:47:06 -0400

    navbar hover

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,21 @@
 function jump(h) {
   var top = document.getElementById(h).offsetTop;
-  window.scrollTo(0, top - 75);
+  window.scrollTo(0, top - 65);
+}
+
+function toggleNavbar(h) {
+  const navbarToggle = document.querySelector(".navbar-toggler");
+  if (navbarToggle) {
+    navbarToggle.click(); // Trigger the collapse toggle
+  }
+  jump(h);
+}
+
+function toggleNavbar(h) {
+  const navbarCollapse = document.querySelector(".navbar-collapse");
+  if (navbarCollapse.classList.contains("show")) {
+    const navbarToggle = document.querySelector(".navbar-toggler");
+    navbarToggle.click(); // Collapse the navbar
+  }
+  jump(h);
 }
