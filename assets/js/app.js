d2abf4d16974ef3452d78454e56ccf5d78080908
Johnny Jiang <johnnyj2608@gmail.com>
2023-08-13 22:32:26 -0400

    Cleaned JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,10 @@
+// Anchors
 function jump(h) {
   var top = document.getElementById(h).offsetTop;
   window.scrollTo(0, top - 45);
 }
 
-function toggleNavbar(h) {
-  const navbarToggle = document.querySelector(".navbar-toggler");
-  if (navbarToggle) {
-    navbarToggle.click(); // Trigger the collapse toggle
-  }
-  jump(h);
-}
-
+// Toggle navbar from navbar-brand
 function toggleNavbar(h) {
   const navbarCollapse = document.querySelector(".navbar-collapse");
   if (navbarCollapse.classList.contains("show")) {
@@ -19,3 +13,31 @@ function toggleNavbar(h) {
   }
   jump(h);
 }
+
+// Transparent navbar while in landing page
+window.addEventListener("scroll", function () {
+  const aboutSection = document.getElementById("about");
+  const navbar = document.querySelector(".navbar");
+
+  if (window.scrollY > aboutSection.offsetTop - 46) {
+    navbar.classList.add("solid-navbar");
+    navbar.classList.remove("transparent-navbar");
+  } else {
+    navbar.classList.remove("solid-navbar");
+    navbar.classList.add("transparent-navbar");
+  }
+});
+
+// Overlay for preloader
+Pace.on("done", function () {
+  var overlay = document.getElementById("overlay");
+  if (overlay) {
+    overlay.style.display = "none"; // Remove preloader
+    document.body.style.overflow = ""; // Re-enable scrolling
+  }
+});
+// Disable scrolling until Pace is done loading
+document.body.style.overflow = "hidden";
+
+// Initializes animate on scroll
+AOS.init();
